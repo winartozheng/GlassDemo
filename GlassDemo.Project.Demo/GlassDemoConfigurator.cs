f531fd75888f2b477f998df429a8c6a88d0c7a4f
Courtney Dean <courtney.dean@perficient.com>
2018-08-16 09:51:55 -0400

    add mvc layout and config to call configurator files for glass mapper and controllers

---
+++ GlassDemo.Project.Demo/GlassDemoConfigurator.cs
@@ -0,0 +1,14 @@
+﻿using GlassDemo.Project.Demo.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace GlassDemo.Project.Demo
+{
+	public class GlassDemoConfigurator : IServicesConfigurator
+	{
+		public void Configure(IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddTransient<GlassDemoController>();
+		}
+	}
+}
