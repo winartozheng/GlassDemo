d35c3a6b00177788a41cee8dee0df59f67537c3f
Courtney Dean <cdean1115@gmail.com>
2018-08-22 20:26:00 -0400

    and SitecoreItem to Demo Article model

--- GlassDemo.Project.Demo/Models/DemoArticle.cs
+++ GlassDemo.Project.Demo/Models/DemoArticle.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Glass.Mapper.Sc.Configuration.Attributes;
 using Glass.Mapper.Sc.Fields;
+using Sitecore.Data.Items;
 
 namespace GlassDemo.Project.Demo.Models
 {
@@ -12,5 +13,8 @@ namespace GlassDemo.Project.Demo.Models
         public virtual string Body { get; set; }
         public virtual Image Image { get; set; }
         public virtual DateTime Date { get; set; }
+
+				[SitecoreItem]
+				public virtual Item Item { get; set; }
     }
 }
